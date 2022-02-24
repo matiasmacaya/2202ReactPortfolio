1f440662c36917c45553fbb9f061829ca9b4e12f
matiasmacaya <matiasmacaya@gmail.com>
2022-02-24 13:15:18 -0300

    Initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,38 @@
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Analyst", "Guru"]
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2></h2>
+          <h1>Matías Macaya</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
